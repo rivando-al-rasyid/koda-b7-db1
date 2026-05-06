7fd4b52344f61ff937be4a410d36da11c48f9a76
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-05-06 16:15:03 +0700

    feat : gendre

--- query2.sql
+++ query2.sql
@@ -1,18 +1,20 @@
 SELECT m.title,
     m.release_date,
     m.rating,
-    d.first_name AS director,
+    d.first_name AS director_full_name,
     g.name AS genre
 FROM movies m
     JOIN directors d ON m.director_id = d.id
     JOIN genres g ON m.genre_id = g.id
-ORDER BY m.rating DESC
+ORDER BY m.rating DESC,
+    m.release_date DESC
 LIMIT 50;
 SELECT m.title,
     m.release_date,
     m.rating,
-    a.first_name AS actor_name
+    a.first_name AS actor_name,
+    ma.role
 FROM movies m
-    JOIN actors a ON m.actor_id = a.id
-ORDER BY m.rating DESC
-LIMIT 50;
+    JOIN movies_actors ma ON m.id = ma.movie_id
+    JOIN actors a ON ma.actor_id = a.id
+ORDER BY m.rating DESC;
